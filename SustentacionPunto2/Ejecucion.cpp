1b297ea7b2b34ca4e1576aca1a31c27b382eb120
liz-yuliana <liz.velandia@urosario.edu.co>
2024-04-08 14:22:06 -0500

    Update

--- SustentacionPunto2/Ejecucion.cpp
+++ SustentacionPunto2/Ejecucion.cpp
@@ -2,107 +2,12 @@
 #include <list>
 #include <time.h>
 #include <random>
+#include "Punto2.cpp"
 using namespace std;
 //lista de frutas y comparar su posicion con el que ingresa el usuarioy si perdio o gano
 
 //adviniar un numero de 3 cifras
-class Frutas{
 
-    public:
-    string frutas [5] = {"Manzana", "Pera", "Durazno", "Uvas", "Arandano"};
-
-    Frutas();
-
-    void busqueda(){
-        string indice;
-        int j = 0, cont = 0;
-
-        cout << "Bienvenido, para ganar tienes que adivinar la fruta en el indice correcto" << endl;
-
-        while (j < frutas->length()){
-            
-            cout << "Ingrese la fruta(Con la primera letra en mayuscula): " << endl;
-            cin >> indice;
-            
-   
-            if(indice == frutas[j]){
-
-                cout << "La fruta Ingresada esta en la posicion correcta" << endl;
-                cont ++;
-            }
-            else{
-
-                cout << "La fruta ingresada esta en otra posicion" << endl;
-
-            }
-
-            j++;
-
-        }
-
-        if(cont == frutas->length()){
-
-            cout << "Felicitaciones. Ganaste" << endl;
-
-        }
-        else{
-
-            cout << "Lo sentimos. Perdiste" << endl;
-
-        }
-    }
-};
-
-
-class Numero{
-
-    public:
-    //srand (time(NULL));
-    int numeroAleatorio = 1000+ rand()% 9999;
-    int numero, counter = 20;
-    
-    void jugar(){
-
-        cout << "Bienvenido, el objetivo del juego es advinar un numero de 3 cifras." << endl;
-
-        while(counter < 20){
-
-            cout << "Tienes 20 intentos y te quedan: " << counter << endl;
-            cout << "Ingrese el numero: " << endl;
-            cin >> numero;
-
-            if(numero > numeroAleatorio){
-
-                cout << "El numero ingresado es mayor que el numero a adivinar" << endl;
-
-            }
-            else if (numero < numeroAleatorio){
-
-                cout << "El numero ingresado es mayor que el numero a adivinar" << endl;
-            }
-            else if(numero == numeroAleatorio){
-
-                cout << "El numero ingresado es igual al numero a adivinar" << endl;
-                cout << "Has ganado. Felicitaciones" << endl;
-                break;
-            }
-
-            counter++;
-
-        }
-
-        if(counter == 20){
-
-            cout << "Has perdido. Lo Sentimos" << endl;
-        
-        }
-
-        
-
-    }
-
-
-};
 
 int main(){
 
@@ -110,7 +15,7 @@ int main(){
     Frutas user;
     Numero user1;
 
-    user.busqueda();
+    //user.busqueda();
     user1.jugar();
 
 }
